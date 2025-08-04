1ad06e0eb2bc88c4ed33bcf220b60afc05f394f9
Charley Bolton <charleybolton1@gmail.com>
2025-08-04 11:51:42 +0100

    👷 ci: add terraform apply job and update backend configuration - add terraform apply job to automatically apply terraform changes on workflow dispatch - update terraform backend configuration to use a new s3 bucket due to AWS account cancellation/reopening

--- infrastructure/terraform/backend.tf
+++ infrastructure/terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket       = "terraform-state-artist-directory"
+    bucket       = "terraform-state-artist-directory-2"
     key          = "artist_directory/dev/terraform.tfstate"
     region       = "eu-west-2"
     encrypt      = true
